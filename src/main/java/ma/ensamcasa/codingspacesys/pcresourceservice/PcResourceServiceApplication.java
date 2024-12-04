4dd5eee4fd234d4486b2bd8f0312c739fd4129e9
LahcenEzzara <114707306+LahcenEzzara@users.noreply.github.com>
2024-12-04 21:16:15 +0100

    First Commit

---
+++ src/main/java/ma/ensamcasa/codingspacesys/pcresourceservice/PcResourceServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.ensamcasa.codingspacesys.pcresourceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcResourceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcResourceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/ensamcasa/codingspacesys/pcresourceservice/PcResourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensamcasa.codingspacesys.pcresourceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcResourceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
